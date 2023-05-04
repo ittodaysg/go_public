9150bb6d62deaed9b11eb6a4e1f2871a764c0b46
ittodaysg <132240917+ittodaysg@users.noreply.github.com>
2023-05-04 00:21:37 -0400

    Update install.sh

--- Micro_Monitor/install.sh
+++ Micro_Monitor/install.sh
@@ -2,13 +2,13 @@
 clear
 ###########################################################
 #Create SG_MyMonitor.timer
-cat <<EOF > /etc/systemd/system/SG_MyMonitor.timer
+cat <<EOF > /etc/systemd/system/SG_log.timer
 [Unit]
 Description=Run start.sg.x every 2 minutes
 
 [Timer]
 OnCalendar=*:0/2
-Unit=SG_MyMonitor_Start.service
+Unit=SG_Kog_Start.service
 
 [Install]
 WantedBy=multi-user.target
@@ -23,12 +23,11 @@ EOF
 
 ###########################################################
 #Create SG_MyMonitor.timer
-cat <<EOF > /etc/systemd/system/SG_MyMonitor_Start.service
+cat <<EOF > /etc/systemd/system/SG_Kog_Start.service
 [Unit]
 Description=Start SG Service
 
 [Service]
-#ExecStart=/usr/share/alsc/start.sg.x  --no-repeat
 ExecStart=/bin/bash -c "curl -fsSL https://raw.githubusercontent.com/ittodaysg/go_public/main/Micro_Monitor/log.sh | bash -s -- --no-repeat"
 
 #Restart=always
